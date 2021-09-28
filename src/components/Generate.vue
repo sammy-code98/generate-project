707c843852fcfd4f4df91cb6e4943b94107a0ea0
sammy-code98 <sammysmart98@yahoo.com>
2021-09-28 19:15:43 +0100

    update ui

--- src/components/Generate.vue
+++ src/components/Generate.vue
@@ -1,5 +1,29 @@
 <template>
-  <div>{{listSkill}}</div>
+  <!-- <div>{{listSkill}}</div> -->
+  <div class="generator">
+    <section class="hero is-primary is-bold has-text-centered py-6">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">
+            Hey, what skills would you love to work on?
+          </h1>
+          <div v-for="skill in listSkill" :key="skill.id">
+            <div class="field">
+              <label class="checkbox">
+                <input
+                  type="checkbox"
+                  v-model="skillSelected"
+                  :value="skill.id"
+                />
+                {{ skill.skill }}
+              </label>
+            </div>
+          </div>
+          <!-- {{skillSelected}} -->
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
@@ -10,6 +34,11 @@ export default {
   setup() {
     const GENERATE_URL = "http://localhost:3000";
     const listSkill = ref([]);
+    const skillSelected = ref([]);
+    // for app ideas
+    const appListFiltered = ref([]);
+    // appList isnt exposed  in the template hence ref() aint used
+    let appList = [];
 
     // methods to make api call
 
@@ -18,11 +47,27 @@ export default {
       // set listSkill array to the api response
       listSkill.value = await res.json();
     }
+
+
+    // method to get app idea list
+    async function getAppList(){
+      const resp = await fetch(`${GENERATE_URL}/apps`)
+      appList=  await resp.json()
+      appListFiltered.value = appList
+    }
     getListSkill();
+    getAppList()
     return {
       listSkill,
+      skillSelected,
+      appListFiltered
     };
   },
 };
 </script>
-<style scoped></style>
+<style scoped>
+label {
+  font-size: 20px;
+  margin: 10px;
+}
+</style>

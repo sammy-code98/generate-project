b7201f518bcda66b8e644932a9f41d21b5e943ce
sammy-code98 <sammysmart98@yahoo.com>
2021-09-28 17:42:52 +0100

    Add api endpoint as submodule

--- src/components/Generate.vue
+++ src/components/Generate.vue
@@ -1,13 +1,28 @@
 <template>
-  <div>dumm</div>
+  <div>{{listSkill}}</div>
 </template>
 
 <script>
+// vue 3 composition api imports
+import { ref } from "vue";
 export default {
-  name:'Generate'
-  
-}
-</script>
-<style scoped>
+  name: "Generate",
+  setup() {
+    const GENERATE_URL = "http://localhost:3000";
+    const listSkill = ref([]);
 
-</style>
+    // methods to make api call
+
+    async function getListSkill() {
+      const res = await fetch(`${GENERATE_URL}/skills`);
+      // set listSkill array to the api response
+      listSkill.value = await res.json();
+    }
+    getListSkill();
+    return {
+      listSkill,
+    };
+  },
+};
+</script>
+<style scoped></style>
